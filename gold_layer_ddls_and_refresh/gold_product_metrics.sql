9c280a643679b4844371fe161dfb02f84da8b2ec
koladevirevanth@gmail.com <koladevirevanth@gmail.com>
2025-12-23 18:44:24 +0000

    delta table fixes

--- gold_layer_ddls_and_refresh/gold_product_metrics.sql
+++ gold_layer_ddls_and_refresh/gold_product_metrics.sql
@@ -1,20 +1,18 @@
-DROP TABLE IF EXISTS ecommerce.gold.revenue_per_product;
-CREATE TABLE ecommerce.gold.revenue_per_product AS
-SELECT product.product_name AS Product,
-SUM(revenue) AS Revenue 
-FROM ecommerce.silver.revenue_per_product_agg product
-GROUP BY Product;
+INSERT OVERWRITE ecommerce.gold.revenue_per_product
+SELECT
+  product_name AS Product,
+  SUM(Revenue) AS Revenue
+FROM ecommerce.silver.revenue_per_product_agg
+GROUP BY product_name;
 
-
-CREATE TABLE ecommerce.gold.product_wise_cancellations AS
-SELECT Product_ID,
-COUNT(DISTINCT order_id) AS Cancellation_Frequency
-SUM(Net_Banking) AS Net_Banking,
-SUM(Card) AS Card,
-SUM(Upi) AS Upi,
-SUM(Quantity) as quantity, 
-SUM(Line_Amount) AS Total_Amount
+INSERT OVERWRITE ecommerce.gold.product_wise_cancellations
+SELECT
+  Product_ID,
+  COUNT(*) AS Cancellation_Frequency,
+  SUM(Net_Banking) AS Net_Banking,
+  SUM(Card) AS Card,
+  SUM(Upi) AS Upi,
+  SUM(Quantity) AS Quantity,
+  SUM(Line_Amount) AS Total_Amount
 FROM ecommerce.silver.cancelled_products_agg
 GROUP BY Product_ID;
-
-
